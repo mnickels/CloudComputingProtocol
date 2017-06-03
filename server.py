27ced42a82893aae7fe849f1aebfd5b4ef50a959
mnickels <mc.nickels2@gmail.com>
2017-06-03 16:40:51 -0700

    Got working for Python 3

--- server.py
+++ server.py
@@ -5,22 +5,17 @@ TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 20
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket()
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
 conn, addr = s.accept()
-print 'Connection address:', addr
-while 1:
-    
+print ('Connection address:', addr)
+while True:
     
     data = conn.recv(BUFFER_SIZE)
-    data1 = int(data) + 1
-    data2 = str(data1)
-    
-    
     
     if not data: break
-    print "received data:", data2
-    conn.send(data2)  # echo
+    print ('received data:', data)
+    conn.send(data)  # echo
 conn.close()
